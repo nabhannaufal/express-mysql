77a575ed3fec04eca1f8f86b40e831699f719d76
Nabhan <muhammad.naufal@phincon.com>
2023-08-15 11:26:09 +0700

    update db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-db.sync()
+db.sync({ force: true })
   .then(() => {
     console.log('Database connected!');
   })
