1abe6decbdddfc5ab7aaba73ee56fb7889e09a1e
Nabhan <muhammad.naufal@phincon.com>
2023-08-15 13:10:23 +0700

    update tmp file

--- routes/galeryRouter.js
+++ routes/galeryRouter.js
@@ -5,7 +5,7 @@ import galeryController from '../controllers/galeryController.js';
 const galeryRouter = express.Router();
 
 galeryRouter.post('/aws', upload.single('photo'), galeryController.addGalery);
-galeryRouter.post('/local', uploadLocal.single('photo'), galeryController.addGaleryLocal);
+galeryRouter.post('/', uploadLocal.single('photo'), galeryController.addGaleryLocal);
 galeryRouter.get('/', galeryController.showGalery);
 galeryRouter.get('/find/:id', galeryController.findPhotos);
 galeryRouter.get('/s3/:key', galeryController.getDataFromS3);
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/api/ping', (req, res) => {
 app.use('/api/movies', movieRouter);
 app.use('/api/galeries', galeryRouter);
 
-app.use(express.static('temp/uploads'));
+app.use(express.static('tmp/uploads'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
